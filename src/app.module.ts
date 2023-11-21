39c127f2b54557459db60b766bcb270ac356ff0d
djelavic_ag <djelavic_ag@medable.com>
2023-11-21 11:52:33 +0100

    Basic auth feature skeleton + user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TasksModule,
+    AuthModule,
     TypeOrmModule.forRoot(typeOrmConfig),
     ConfigModule.forRoot(),
   ],
