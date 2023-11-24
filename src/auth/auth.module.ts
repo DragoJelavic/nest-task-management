8e52ea91e86b78821ae4c6a4cb3c7320fc94ea2b
djelavic_ag <djelavic_ag@medable.com>
2023-11-24 15:41:44 +0100

    remove handling env vars from module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config'; // Import ConfigModule and ConfigService
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersRepository } from './users.repository';
@@ -7,24 +6,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
-        signOptions: {
-          expiresIn: configService.get<number>('JWT_EXPIRY_IN_SECONDS'),
-        },
-      }),
-      inject: [ConfigService],
-    }),
+    JwtModule.register({}),
     TypeOrmModule.forFeature([User]),
   ],
-  exports: [TypeOrmModule],
-  providers: [AuthService, UsersRepository],
+  exports: [TypeOrmModule, PassportModule],
+  providers: [AuthService, UsersRepository, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
